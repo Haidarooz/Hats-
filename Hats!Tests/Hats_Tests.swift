9be7336ef8c4e903a559f333b2f9458d4028fa47
Haidar Mohammed <Haidarooz@haidars-macbook-pro.local>
2019-02-05 13:15:23 +1100

    Initial Commit

---
+++ Hats!Tests/Hats_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Hats_Tests.swift
+//  Hats!Tests
+//
+//  Created by Haidar Mohammed on 5/2/19.
+//  Copyright © 2019 Haidar AlOgaily. All rights reserved.
+//
+
+import XCTest
+@testable import Hats_
+
+class Hats_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
